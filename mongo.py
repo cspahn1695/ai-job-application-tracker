f8980a1ef5de5f3d9be108c4d021962e73cf3a3d
cspahn1695 <christopher-spahn@uiowa.edu>
2026-04-16 14:16:57 -0500

    updated connection string for Atlas DB so we call all work on it

--- mongo.py
+++ mongo.py
@@ -6,7 +6,7 @@ from user_model import User
 from background_model import Background
 
 async def init_mongo():
-    client = AsyncIOMotorClient("mongodb://localhost:27017/")
+    client = AsyncIOMotorClient("mongodb+srv://chriss:VRf4H6BafmZbe8C@cluster0.acnynb2.mongodb.net/?appName=Cluster0")
     db = client["job_app_db"]
 
     print("✅ Connecting to MongoDB...")
